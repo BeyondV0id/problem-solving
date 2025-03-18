98f9537b357467e18da6c0e18fdb35e4b1f6ddb4
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-03-18 21:49:05 +0530

    File updated at 3/18/2025, 9:49:07 PM

--- 61-RotateList/61-RotateList.cpp
+++ 61-RotateList/61-RotateList.cpp
@@ -19,18 +19,20 @@ public:
         temp = temp->next;
         size++; 
         }
-        k = k%size;
+        //point temp to last node
+        k = k%size;//check to mod size
         if(k==0)return head;
-        int lastIndex = size-k;
+        int lastIndex = size-k;//get index where last element would be after rotation..
         ListNode* temp2 = head;
         for(int i=1;i<lastIndex;i++){
             temp2=temp2->next;
         }
-        ListNode *newNode = temp2->next;
-        temp2->next = nullptr;
-        temp->next = head;
+        //temp2 points to the node that would be new last node
+        ListNode *newHead = temp2->next;//newHead will point to next index of temp2
+        temp2->next = nullptr;//make temp2 null as it is last
+        temp->next = head;//dont forget to connect temp(last node of normal list) to head of normal linked list
 
-        
-        return newNode;   
+
+        return newHead;   
     }
 };
