d2b4fcd09a688fa6f35ea6467f1311b85711ffed
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-03-21 22:19:11 +0530

    File updated at 3/21/2025, 10:19:14 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,15 +1,15 @@
-// Last updated: 3/21/2025, 10:14:36 PM
+// Last updated: 3/21/2025, 10:19:14 PM
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>freq;
+        unordered_map<int,bool>seen;
         for(int num:nums){
-            freq[num]++;
-        }
-        for(auto num:nums){
-            if(freq[num]>1)return true;
+            if(seen[num])return true;
+            seen[num] = true;
         }
         return false;
+
+    
         
     }
 };
