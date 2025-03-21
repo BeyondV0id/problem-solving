b2b9973a539c4d9e3f8c4d1328ba5bdbfe32c028
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-03-21 19:12:03 +0530

    File updated at 3/21/2025, 7:12:07 PM

--- 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.cpp
+++ 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.cpp
@@ -1,4 +1,4 @@
-// Last updated: 3/21/2025, 6:37:19 PM
+// Last updated: 3/21/2025, 7:12:07 PM
 class Solution {
 public:
     bool check(vector<int>& nums) {
@@ -11,7 +11,9 @@ public:
             }
            
         }
-        if(nums[nums.size()-1]>nums[0])isRotated++;
+        if(nums[nums.size()-1]>nums[0])isRotated++;//if the array is neither  sorted nor rotated 
+        // example test case {2 1 3 4}: here 2 cannot be at front if it is rotated as 2<4.
+        //(2,|1,3,4|) is sorted but not perfectely rotated
         return isRotated<=1;
     }
 };
