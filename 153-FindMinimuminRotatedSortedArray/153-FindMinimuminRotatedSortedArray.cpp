c320a0a5593918bb333a817dd3da98aaeabbbdc2
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-03-23 10:27:48 +0530

    File updated at 3/23/2025, 10:27:52 AM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
@@ -1,22 +1,19 @@
-// Last updated: 3/23/2025, 10:27:05 AM
+// Last updated: 3/23/2025, 10:27:52 AM
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int l=0,r=nums.size()-1;
-        int minimum=INT_MAX;
-        while(l<=r){
-            int m = l+(r-l)/2;
-            if(nums[l]<=nums[m]){
-                minimum = min(nums[l],minimum);
-                l = m+1;
+        int l = 0, r = nums.size() - 1;
+        int minimum = INT_MAX;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[l] <= nums[m]) {
+                minimum = min(nums[l], minimum);
+                l = m + 1;
+            } else {
+                minimum = min(nums[m], minimum);
+                r = m - 1;
             }
-            else{
-                minimum = min(nums[m],minimum);
-                r = m-1;
-            }
-
         }
         return minimum;
-        
     }
 };
