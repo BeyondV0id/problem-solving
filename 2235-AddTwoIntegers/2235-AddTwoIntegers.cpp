dab5c5abf0b05fb9fc172a969ad6a6e120376345
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-03-31 15:26:29 +0530

    File updated at 3/31/2025, 3:26:30 PM

--- 2235-AddTwoIntegers/2235-AddTwoIntegers.cpp
+++ 2235-AddTwoIntegers/2235-AddTwoIntegers.cpp
@@ -1,8 +1,7 @@
-// Last updated: 3/31/2025, 3:25:36 PM
+// Last updated: 3/31/2025, 3:26:30 PM
 class Solution {
 public:
     int sum(int num1, int num2) {
-        return num1+num2;
-        
+        return num1 + num2;
     }
 };
