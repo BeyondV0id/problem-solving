64be6705e1a44b3225bc03c2df832c9350f0b865
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-01 18:25:43 +0530

    Time: 3 ms (76.55%) | Memory: 28.5 MB (63.12%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=503 lang=cpp
+ *
+ * [503] Next Greater Element II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        stack<int>st;
+        vector<int>result(n,-1);
+        for(int i=0;i<2*n;i++){
+            int index = i%n;
+            while(!st.empty() && nums[st.top()]<nums[index]){
+                result[st.top()] = nums[index];
+                st.pop();
+            }
+            st.push(index);
+        }
+        
+        return result;
+        
+    }
+};
+// @lc code=end
+
