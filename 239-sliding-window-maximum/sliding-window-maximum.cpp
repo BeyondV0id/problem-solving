83fcabf30b2a986fcfce917720d82fbf4dfb6b21
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-02 19:17:16 +0530

    Time: 27 ms (49.53%) | Memory: 139.2 MB (39.03%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        deque<int> deadpool; 
+
+        for (int i = 0; i < nums.size(); i++) {
+            // Remove elements out of window bounds
+            bool isOutOfBounds = !deadpool.empty() && deadpool.front() <= i - k;
+            if (isOutOfBounds) {
+                deadpool.pop_front();
+            }
+
+            // Remove smaller elements (they can't be max)
+            while (!deadpool.empty() && nums[deadpool.back()] < nums[i]) {
+                deadpool.pop_back();
+            }
+
+            deadpool.push_back(i);
+
+            // If we have a full window, add the max to result
+            bool isWindowFilled = i >= k - 1;
+            if (isWindowFilled) {
+                result.push_back(nums[deadpool.front()]);
+            }
+        }
+        return result;
+    }
+};
