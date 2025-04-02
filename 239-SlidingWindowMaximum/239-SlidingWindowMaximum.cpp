f2c8a365315c10c5ca58fab36d8c322c9a5c4383
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-02 19:17:12 +0530

    File updated at 4/2/2025, 7:17:14 PM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -1,32 +1,30 @@
-// Last updated: 4/2/2025, 5:16:58 PM
-/*
- * @lc app=leetcode id=4 lang=cpp
- *
- * [4] Median of Two Sorted Arrays
- */
-
-// @lc code=start
+// Last updated: 4/2/2025, 7:17:14 PM
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>merged(nums1.size()+nums2.size());
-        int m = 0,n = 0,k=0;
-        while(m<nums1.size() && n < nums2.size()){
-            if(nums1[m] <= nums2[n])
-                merged[k++] = nums1[m++]; 
-            else if(nums2[n] <= nums1[m]) 
-                merged[k++] = nums2[n++];
-        }
-        while(m<nums1.size())
-            merged[k++] = nums1[m++];
-        while(n<nums2.size())
-            merged[k++]  = nums2[n++];
-        int length = merged.size();
-        if(length %2!= 0)
-             return merged[length/2];
-        else
-            return (merged[length/2]+merged[length/2-1])/2.0;
-}
-};
-// @lc code=end
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        deque<int> deadpool; 
 
+        for (int i = 0; i < nums.size(); i++) {
+            // Remove elements out of window bounds
+            bool isOutOfBounds = !deadpool.empty() && deadpool.front() <= i - k;
+            if (isOutOfBounds) {
+                deadpool.pop_front();
+            }
+
+            // Remove smaller elements (they can't be max)
+            while (!deadpool.empty() && nums[deadpool.back()] < nums[i]) {
+                deadpool.pop_back();
+            }
+
+            deadpool.push_back(i);
+
+            // If we have a full window, add the max to result
+            bool isWindowFilled = i >= k - 1;
+            if (isWindowFilled) {
+                result.push_back(nums[deadpool.front()]);
+            }
+        }
+        return result;
+    }
+};
