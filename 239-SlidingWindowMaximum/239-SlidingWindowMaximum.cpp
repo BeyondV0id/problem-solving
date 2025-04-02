86030e1f2d915004364bf058d6f15d276e73e1f0
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-02 19:18:04 +0530

    File updated at 4/2/2025, 7:18:06 PM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/2/2025, 7:17:14 PM
+// Last updated: 4/2/2025, 7:18:06 PM
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -6,20 +6,15 @@ public:
         deque<int> deadpool; 
 
         for (int i = 0; i < nums.size(); i++) {
-            // Remove elements out of window bounds
-            bool isOutOfBounds = !deadpool.empty() && deadpool.front() <= i - k;
+            bool isOutOfBounds = !deadpool.empty() && k <= i - deadpool.front();
             if (isOutOfBounds) {
                 deadpool.pop_front();
             }
-
-            // Remove smaller elements (they can't be max)
             while (!deadpool.empty() && nums[deadpool.back()] < nums[i]) {
                 deadpool.pop_back();
             }
 
             deadpool.push_back(i);
-
-            // If we have a full window, add the max to result
             bool isWindowFilled = i >= k - 1;
             if (isWindowFilled) {
                 result.push_back(nums[deadpool.front()]);
