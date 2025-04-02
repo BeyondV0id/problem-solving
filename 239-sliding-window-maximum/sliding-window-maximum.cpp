8c3f58ccb87c7dfae7560383ffa2dec23712cbdd
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-02 19:18:06 +0530

    Time: 30 ms (40.34%) | Memory: 139.2 MB (39.03%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -5,20 +5,15 @@ public:
         deque<int> deadpool; 
 
         for (int i = 0; i < nums.size(); i++) {
-            // Remove elements out of window bounds
-            bool isOutOfBounds = !deadpool.empty() && deadpool.front() <= i - k;
+            bool isOutOfBounds = !deadpool.empty() && k <= i - deadpool.front();
             if (isOutOfBounds) {
                 deadpool.pop_front();
             }
-
-            // Remove smaller elements (they can't be max)
             while (!deadpool.empty() && nums[deadpool.back()] < nums[i]) {
                 deadpool.pop_back();
             }
 
             deadpool.push_back(i);
-
-            // If we have a full window, add the max to result
             bool isWindowFilled = i >= k - 1;
             if (isWindowFilled) {
                 result.push_back(nums[deadpool.front()]);
