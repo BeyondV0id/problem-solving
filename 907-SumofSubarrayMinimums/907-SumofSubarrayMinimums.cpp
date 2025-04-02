91a02a420af429b2a401d3444af8c96c9c31016a
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-02 20:35:00 +0530

    Create file

---
+++ 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.cpp
@@ -0,0 +1,21 @@
+// Last updated: 4/2/2025, 8:35:01 PM
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int>lesser;
+        vector<int>middle;
+        vector<int>greater;
+        for(int num:nums){
+            if(num<pivot) lesser.push_back(num);
+            else if(num == pivot) middle.push_back(num);
+            else greater.push_back(num);
+        }
+        vector<int>result;
+        result.insert(result.end(),lesser.begin(),lesser.end());
+        result.insert(result.end(),middle.begin(),middle.end());
+        result.insert(result.end(),greater.begin(),greater.end());
+        return result;
+
+        
+    }
+};
