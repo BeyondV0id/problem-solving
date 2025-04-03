f7bc5db7d858e12257ea720d533da56c58045688
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-03 20:54:47 +0530

    Create file

---
+++ 2418-SortthePeople/2418-SortthePeople.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/3/2025, 8:54:49 PM
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int,string>> heightPairs;
+        for(int i=0;i<heights.size();i++){
+            heightPairs.push_back({heights[i],names[i]});
+        }
+        sort(heightPairs.rbegin(),heightPairs.rend());
+        vector<string>sortedNames;
+        for(int i=0;i<heights.size();i++){
+            sortedNames.push_back(heightPairs[i].second);
+        }
+        return sortedNames;
+
+        
+    }
+};
