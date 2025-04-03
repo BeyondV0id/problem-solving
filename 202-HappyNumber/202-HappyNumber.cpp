479485c6cfe16c76a0b6b7c6a2f55cdb41cca47e
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-03 12:57:08 +0530

    Create file

---
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -0,0 +1,27 @@
+// Last updated: 4/3/2025, 12:56:55 PM
+class Solution {
+public:
+    bool isHappy(int n) {
+        if (n == 1)
+            return true;
+        int slow, fast;
+        slow = n;
+        fast = sumSquares(n);
+        while (fast != 1 && slow != fast) {
+            slow = sumSquares(slow);
+            fast = sumSquares(sumSquares(fast));
+        }
+        return fast==1;
+    }
+    int sumSquares(int n) {
+        if (n == 1)
+            return 1;
+        int sumOfSquares = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            sumOfSquares += rem * rem;
+            n = n / 10;
+        }
+        return sumOfSquares;
+    }
+};
