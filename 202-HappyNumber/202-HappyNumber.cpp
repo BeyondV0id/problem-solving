c072e249c2c06e802155707e327772e875e4720b
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-03 13:04:22 +0530

    File updated at 4/3/2025, 1:04:18 PM

--- 202-HappyNumber/202-HappyNumber.cpp
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -1,27 +1,68 @@
-// Last updated: 4/3/2025, 12:56:55 PM
+// Last updated: 4/3/2025, 1:04:18 PM
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+int len(int n) {
+    int l = 0;
+    while(n) {
+        l++;
+        n /= 10;
+    }
+    return l;
+}
+
+class Solution_ {
+public:
+bool isHappy(int n) {
+    std::set<int> seen;
+    seen.insert(n);
+    while(1) {
+        if(n == 1) {
+            return true;
+        }
+  
+        int new_num = 0;
+        while(n) {
+            int r = n % 10;
+            new_num += (r * r);
+            n /= 10;
+        }
+        n = new_num;
+        auto pr = seen.insert(n);
+        if(pr.second == false) {
+            break;
+        }
+    }   
+    return false;
+}
+
+};
+
+int sum_square(int n) {
+    int sum = 0;
+    while(n) {
+        int t = n % 10;
+        sum += (t * t);
+        n /= 10;
+    }
+    return sum;
+}
+
 class Solution {
 public:
-    bool isHappy(int n) {
-        if (n == 1)
-            return true;
-        int slow, fast;
-        slow = n;
-        fast = sumSquares(n);
-        while (fast != 1 && slow != fast) {
-            slow = sumSquares(slow);
-            fast = sumSquares(sumSquares(fast));
-        }
-        return fast==1;
-    }
-    int sumSquares(int n) {
-        if (n == 1)
-            return 1;
-        int sumOfSquares = 0;
-        while (n > 0) {
-            int rem = n % 10;
-            sumOfSquares += rem * rem;
-            n = n / 10;
-        }
-        return sumOfSquares;
-    }
+bool isHappy(int n) {
+    int slow = n;
+    int fast = n;
+    
+    do {
+       slow = sum_square(slow);
+       fast = sum_square(sum_square(fast));
+        
+    }   while(slow != fast);
+    return slow == 1;
+}
+
 };
