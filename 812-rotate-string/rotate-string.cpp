1e46f52804a1ef476d331840add51016711967cf
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-04 17:47:05 +0530

    Time: 1 ms (14.28%) | Memory: 8.7 MB (14.22%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=796 lang=cpp
+ *
+ * [796] Rotate String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size())return false;
+
+        for(int i=0;i<s.size();i++){
+            string rotated;
+            for(int j=0;j<s.size();j++){
+                int newIndex = (j+i)%s.size();
+                rotated+=s[newIndex];
+            }
+            if(rotated == goal)return true;
+    }
+    return false;
+    }
+
+};
+// @lc code=end
+
