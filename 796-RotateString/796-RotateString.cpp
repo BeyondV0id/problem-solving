371ac2e3d2cf03490a3d505ba87265759a075e9e
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-04 17:47:01 +0530

    Create file

---
+++ 796-RotateString/796-RotateString.cpp
@@ -0,0 +1,27 @@
+// Last updated: 4/4/2025, 5:47:04 PM
+/*
+ * @lc app=leetcode id=796 lang=cpp
+ *
+ * [796] Rotate String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size())return false;
+
+        for(int i=0;i<s.size();i++){
+            string rotated;
+            for(int j=0;j<s.size();j++){
+                int newIndex = (j+i)%s.size();
+                rotated+=s[newIndex];
+            }
+            if(rotated == goal)return true;
+    }
+    return false;
+    }
+
+};
+// @lc code=end
+
