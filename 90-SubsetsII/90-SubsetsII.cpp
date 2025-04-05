d8a96fbe5b86f956b25f2d895ae5c2973224733c
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-05 11:56:07 +0530

    File updated at 4/5/2025, 11:56:08 AM

--- 90-SubsetsII/90-SubsetsII.cpp
+++ 90-SubsetsII/90-SubsetsII.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/5/2025, 11:56:00 AM
+// Last updated: 4/5/2025, 11:56:08 AM
 /*
 case 1:
  { },
@@ -31,13 +31,6 @@ but at      {1 , 2}        and       {1 , 3}
 
 skip if i not eqaul to index and nums[i] == nums[i-1]//sorted
 
-
- { },
- { 1 },
- { 1 2 },
- { 1 2 2 },
- { 2 },
- { 2 2 },
  */
 
 class Solution {
