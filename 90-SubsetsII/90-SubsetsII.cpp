b6a14a2e6fe326d255f48702d69b118642426e59
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-05 11:55:59 +0530

    File updated at 4/5/2025, 11:56:00 AM

--- 90-SubsetsII/90-SubsetsII.cpp
+++ 90-SubsetsII/90-SubsetsII.cpp
@@ -1,5 +1,45 @@
-// Last updated: 4/5/2025, 11:48:31 AM
-//vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+// Last updated: 4/5/2025, 11:56:00 AM
+/*
+case 1:
+ { },
+ { 1 },
+ { 1 2 },
+ { 1 2 3 },
+ { 1 3 },
+ { 2 },
+ { 2 3 },
+ { 3 },
+
+case 2:
+ { },
+ { 1 },
+ { 1 2 },
+ { 1 2 2 },
+ { 2 },
+ { 2 2 },
+
+if we replace 3 to 2
+1 2 2 is fine
+
+but 1 2 and 1 2 is duplicate...
+
+at 1 2 3 each step index == i
+
+but at      {1 , 2}        and       {1 , 3}
+    index = 0,i = 1          index = 0,i = 2
+
+
+skip if i not eqaul to index and nums[i] == nums[i-1]//sorted
+
+
+ { },
+ { 1 },
+ { 1 2 },
+ { 1 2 2 },
+ { 2 },
+ { 2 2 },
+ */
+
 class Solution {
 private:
     void backtrack(vector<int>& nums, int index, vector<int>& current,vector<vector<int>>& result) {
