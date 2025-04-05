a4644e6d45f238f9d0286cf9f3b0379845e07f66
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-05 09:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (37.83%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -11,8 +11,8 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = grid[0][0];
         // base case
+        dp[0][0] = grid[0][0];
         for (int i = 1; i < m; i++)
             dp[i][0] = dp[i][0] = grid[i][0] + dp[i - 1][0];
         for (int j = 1; j < n; j++)
