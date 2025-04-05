6298b8730e31ce068a64a97cdda6d22c51245b75
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-05 09:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (89.30%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s = to_string(num);
+        bool swapped = false;
+        for (int i = 0; i < s.size() - 1 && !swapped; i++) {
+            int max_indx = i;
+            for (int j = i + 1; j < s.size(); j++) {
+                if (s[j] > s[i] && s[j] >= s[max_indx]) {
+                    max_indx = j;
+                }
+            }
+            if (i != max_indx) {
+            swap(s[i], s[max_indx]);
+            swapped = true;
+        }
+        }
+        int result = stoi(s);
+        return result;
+    }
+};
