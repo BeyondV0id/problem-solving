e7cc3b7ca8c31fc7f59847f7b515a8bc698de601
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-05 09:12:34 +0530

    File updated at 4/5/2025, 9:12:35 AM

--- 64-MinimumPathSum/64-MinimumPathSum.cpp
+++ 64-MinimumPathSum/64-MinimumPathSum.cpp
@@ -1,30 +1,37 @@
-// Last updated: 4/5/2025, 9:11:31 AM
-/*
- * @lc app=leetcode id=64 lang=cpp
- *
- * [64] Minimum Path Sum
- */
-
-// @lc code=start
+// Last updated: 4/5/2025, 9:12:35 AM
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = grid[0][0];
-        // base case
-        for (int i = 1; i < m; i++)
-            dp[i][0] = dp[i][0] = grid[i][0] + dp[i - 1][0];
-        for (int j = 1; j < n; j++)
-            dp[0][j] = dp[0][j] = grid[0][j] + dp[0][j - 1];
-        // dp[i][j] = grid[i][j]+ min(dp[i-1][j],dp[i][j-1])
-        for (int i = 1; i < m; i++)
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
-            }
+    static constexpr int outOfBounds{-1};
 
-        return dp[m - 1][n - 1];
+    int minPathSum(vector<vector<int>>& grid) {
+        vector<vector<bool>> mem(grid.size());
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                mem[i].push_back(false);
+            }
+        }
+
+        return findPath(grid, mem);
+    }
+
+    int findPath(vector<vector<int>>& grid, vector<vector<bool>>& mem,
+                 int i = 0, int j = 0) {
+        if (i >= grid[0].size() || j >= grid.size())
+            return outOfBounds;
+        else if (i == grid[0].size() - 1 && j == grid.size() - 1)
+            return grid[j][i];
+        else if (mem[j][i])
+            return grid[j][i];
+
+        int right = findPath(grid, mem, i + 1, j);
+        int left = findPath(grid, mem, i, j + 1);
+        mem[j][i] = true;
+        
+        if (right == outOfBounds)
+            return grid[j][i] += left;
+        else if (left == outOfBounds)
+            return grid[j][i] += right;
+        else
+            return left < right ? (grid[j][i] += left) : (grid[j][i] += right);
     }
 };
-// @lc code=end
