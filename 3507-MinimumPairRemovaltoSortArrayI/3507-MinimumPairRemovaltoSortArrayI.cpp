f2f6ff5286f37401f9d384662892ee196cc50044
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-06 12:49:27 +0530

    Create file

---
+++ 3507-MinimumPairRemovaltoSortArrayI/3507-MinimumPairRemovaltoSortArrayI.cpp
@@ -0,0 +1,41 @@
+// Last updated: 4/6/2025, 12:49:28 PM
+class Solution {
+
+private:
+    void shiftLeft(vector<int>& nums, int index) {
+        for (int i = index; i < nums.size() - 1; i++) {
+            nums[i] = nums[i + 1];
+        }
+        nums.pop_back();
+    }
+    void mergeMinPair(vector<int>& nums) {
+        //search for min Sum
+        int minSum = INT_MAX;
+        int minIndx = 0;
+        for (int i = 0; i < nums.size()-1; i++) {
+            int sum = nums[i] + nums[i + 1];
+            if (sum < minSum) {
+                minIndx = i;
+                minSum = sum;
+            }
+        }
+        nums[minIndx] = minSum;
+        shiftLeft(nums, minIndx + 1);
+    }
+    bool isSorted(vector<int>&nums){
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1])return false;
+        }
+        return true;
+    }
+
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        int cnt = 0;
+        while (!isSorted(nums)) {
+            mergeMinPair(nums);
+            cnt++;
+        }
+        return cnt;
+    }
+};
