59cc0ac8663ff8b7519075cb7ae8ac473b408ff3
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-06 19:04:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (34.10%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,8 +5,8 @@ public:
         dp[0] = 1;
 
         for (int i = 0; i <= n-1; i++) {
-            dp[i + 1] += dp[i];
-            dp[i + 2] += dp[i];
+            dp[i + 1] = dp[i] + dp[i+1];
+            dp[i + 2] = dp[i] + dp[i+2];
         }
 
         return dp[n];
