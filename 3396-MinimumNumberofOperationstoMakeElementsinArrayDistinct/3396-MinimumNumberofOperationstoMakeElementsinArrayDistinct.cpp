3be3789b611602db0a19770a55487048ab46c043
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-08 16:58:12 +0530

    File updated at 4/8/2025, 4:58:12 PM

--- 3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.cpp
+++ 3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.cpp
@@ -1,31 +1,14 @@
-// Last updated: 4/8/2025, 4:50:15 PM
+// Last updated: 4/8/2025, 4:58:12 PM
 class Solution {
-private:
-    void removeElements(vector <int>& nums) {
-        int size = nums.size();
-        int limit = min(3,size);
-        for (int i = 0; i < limit; i++) {
-            nums.erase(nums.begin());
-        }
-    }
-
-    bool isUnique(vector<int>& nums) {
-        unordered_set<int> freq;
-        for (int i = 0; i < nums.size(); i++) {
-            if (freq.count(nums[i]))
-                return false;
-            freq.insert(nums[i]);
-        }
-        return true;
-    }
-
 public:
     int minimumOperations(vector<int>& nums) {
-        int cnt = 0;
-        while (!isUnique(nums)) {
-            removeElements(nums);
-            cnt++;
+        map<int,int>mp;
+        int i;
+        for( i=nums.size()-1;i>=0;i--){
+            if(mp[nums[i]]>0)break;
+            mp[nums[i]]++;
         }
-        return cnt;
+        int a=ceil((i+1)/3.0);
+        return a;
     }
 };
