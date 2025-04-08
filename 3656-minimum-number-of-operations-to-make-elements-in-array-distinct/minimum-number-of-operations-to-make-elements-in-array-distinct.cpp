2b5124e336aca59cc409c0d467c5765fc3bedb2a
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-08 16:50:18 +0530

    Time: 20 ms (16.21%) | Memory: 31.4 MB (18.60%) - LeetSync

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    void removeElements(vector <int>& nums) {
+        int size = nums.size();
+        int limit = min(3,size);
+        for (int i = 0; i < limit; i++) {
+            nums.erase(nums.begin());
+        }
+    }
+
+    bool isUnique(vector<int>& nums) {
+        unordered_set<int> freq;
+        for (int i = 0; i < nums.size(); i++) {
+            if (freq.count(nums[i]))
+                return false;
+            freq.insert(nums[i]);
+        }
+        return true;
+    }
+
+public:
+    int minimumOperations(vector<int>& nums) {
+        int cnt = 0;
+        while (!isUnique(nums)) {
+            removeElements(nums);
+            cnt++;
+        }
+        return cnt;
+    }
+};
