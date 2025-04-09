f37cc9a5d91627688578c6a56ef785d42106c53e
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-09 17:29:57 +0530

    Create file

---
+++ 3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/9/2025, 5:29:57 PM
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        vector<int> freq(101, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < k)
+                return -1;
+            if (nums[i] > k)
+                freq[nums[i]]++;
+        }
+        int cnt = 0;
+        for (int i = 0; i < 101; i++) {
+            if (freq[i] > 0)
+                cnt++;
+        }
+        return cnt;
+    }
+};
