5159f41907d149d42a170018deaec17299d7451c
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-10 12:19:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (70.07%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=46 lang=cpp
+ *
+ * [46] Permutations
+ */
+
+// @lc code=start
+class Solution {
+private:
+    void backtrack(vector<int>&nums,vector<bool>&visited,
+        vector<int>&solve,vector<vector<int>>&result){
+        if(solve.size()==nums.size()){
+            result.push_back(solve);
+            return;
+        }
+
+        for(int i = 0;i<nums.size();i++){
+            if(visited[i])continue;
+            visited[i] = true;
+            solve.push_back(nums[i]);
+            backtrack(nums,visited,solve,result);
+
+
+            solve.pop_back();
+            visited[i] = false;
+            
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>sol;
+        vector<bool>visited(nums.size(),false);
+        vector<vector<int>>result;
+        backtrack(nums,visited,sol,result);
+        return result;
+
+        
+    }
+};
+// @lc code=end
+
