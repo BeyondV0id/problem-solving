55de6819510cc34f68dfd47418aa2d4ba7c00c0f
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-11 18:09:56 +0530

    Create file

---
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/11/2025, 6:09:57 PM
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cnt = 0;
+        long long sum = 0;
+        unordered_map<long long, int> preSumMap;
+        preSumMap[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            long long rem = sum - k;
+            if (preSumMap.find(rem) != preSumMap.end()) {
+                cnt += preSumMap[rem];
+            }
+            preSumMap[sum]++;
+        }
+        return cnt;
+    }
+};
