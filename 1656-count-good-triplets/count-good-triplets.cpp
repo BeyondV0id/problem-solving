5797067f5f86699d59f8291224910e3a59296529
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-14 07:44:02 +0530

    Time: 6 ms (78.59%) | Memory: 11.1 MB (43.88%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -5,12 +5,12 @@ public:
         for (int i = 0; i < arr.size() - 2; i++)
             for (int j = i + 1; j < arr.size() - 1; j++)
                 for (int k = j + 1; k < arr.size(); k++) {
-                    int firstPair = abs(arr[i]-arr[j]);
-                    int secondPair = abs(arr[j]-arr[k]);
-                    int thirdPair = abs(arr[i]-arr[k]);
-                    if(firstPair<=a && secondPair <=b && thirdPair <= c)
-                        cnt++;
-
+                    if ((abs(arr[i] - arr[j]) <= a) &&
+                        (abs(arr[j] - arr[k]) <= b) &&
+                        (abs(arr[i] - arr[k]) <= c) && i != j && j != k &&
+                        i != k){
+                            cnt++;
+                        }
                 }
         return cnt;
     }
