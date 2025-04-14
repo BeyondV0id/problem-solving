0c87d2cbf78c065602cc0930e68d34be4b65792a
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-14 07:44:00 +0530

    File updated at 4/14/2025, 7:43:59 AM

--- 1534-CountGoodTriplets/1534-CountGoodTriplets.cpp
+++ 1534-CountGoodTriplets/1534-CountGoodTriplets.cpp
@@ -1,18 +1,18 @@
-// Last updated: 4/14/2025, 7:42:22 AM
+// Last updated: 4/14/2025, 7:43:59 AM
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int n = arr.size();
         int cnt = 0;
-        for (int i = 0; i < arr.size() - 2; i++)
-            for (int j = i + 1; j < arr.size() - 1; j++)
-                for (int k = j + 1; k < arr.size(); k++) {
-                    int firstPair = abs(arr[i]-arr[j]);
-                    int secondPair = abs(arr[j]-arr[k]);
-                    int thirdPair = abs(arr[i]-arr[k]);
-                    if(firstPair<=a && secondPair <=b && thirdPair <= c)
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                for(int k = j+1 ; k < n ; k++){
+                    if((abs(arr[i] - arr[j]) <= a) && (abs(arr[j] - arr[k]) <= b) && (abs(arr[i] - arr[k]) <= c) && i!=j && j!=k && i!=k){
                         cnt++;
-
+                    }
                 }
+            }
+        }
         return cnt;
     }
 };
