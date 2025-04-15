6b552e0fcc8ff543725ecce550693b22e6d85891
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-15 15:17:25 +0530

    Time: 33 ms (19.99%) | Memory: 88.3 MB (45.77%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -3,16 +3,15 @@ public:
     int findMaxLength(vector<int>& nums) {
         unordered_map<int, int> indxMap;
         indxMap[0] = -1;
-        int cnt1 = 0, cnt2 = 0;
+        int diff  = 0;
+        indxMap[diff] = -1;
         int maxLen = 0;
 
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == 1)
-                cnt1++;
+                diff++;
             else
-                cnt2++;
-            int diff = cnt2 - cnt1;
-
+                diff--;
             if (indxMap.count(diff))
                 maxLen = max(maxLen, i - indxMap[diff]);
             if (indxMap.find(diff) == indxMap.end()) {//the diff is not in the map....
