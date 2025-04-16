d68414ca5231cca10a9e6419c749f4833710cf76
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-16 12:09:05 +0530

    Create file

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/16/2025, 12:09:05 PM
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+
+        unordered_map<char, char> s_t;
+        unordered_map<char, char> t_s;
+        for (int i = 0; i < s.size(); i++) {
+            char sChar = s[i];
+            char tChar = t[i];
+            if (s_t.count(sChar) && s_t[sChar] != tChar)
+                return false;
+            else if (t_s.count(tChar) && t_s[tChar] != sChar)
+                return false;
+            s_t[sChar] = tChar;
+            t_s[tChar] = sChar;
+        }
+        return true;
+    }
+};
