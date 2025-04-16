f7f6891c844b0dbda7d9e9fe335c1c1697e34da7
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-16 16:41:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (42.46%) - LeetSync

---
+++ 2602-maximum-enemy-forts-that-can-be-captured/maximum-enemy-forts-that-can-be-captured.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int captureForts(std::vector<int>& forts) {
+        int maxDistance = 0;
+        int count;
+        int left = 0, n = forts.size();
+
+        while (left < n) {
+            while (left < n && forts[left] == 0)
+                left++;
+
+            int right = left + 1;
+
+            while (right < n && forts[right] == 0)
+                right++;
+            if (right < n && forts[left] + forts[right] == 0) {
+                bool valid_path = true;
+                for (int i = left + 1; i < right; i++) {
+                    if (forts[i] != 0) {
+                        valid_path = false;
+                        break;
+                    }
+                }
+                if (valid_path) {
+                    maxDistance = max(maxDistance, right - left - 1);
+                }
+            }
+
+            left = right;
+        }
+
+        return maxDistance;
+    }
+};
