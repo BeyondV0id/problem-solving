54555dc89502364be4aaf59b15b7bd06910288d7
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-16 16:41:33 +0530

    File updated at 4/16/2025, 4:41:33 PM

--- 2511-MaximumEnemyFortsThatCanBeCaptured/2511-MaximumEnemyFortsThatCanBeCaptured.cpp
+++ 2511-MaximumEnemyFortsThatCanBeCaptured/2511-MaximumEnemyFortsThatCanBeCaptured.cpp
@@ -1,3 +1,4 @@
+// Last updated: 4/16/2025, 4:41:33 PM
 class Solution {
 public:
     int captureForts(std::vector<int>& forts) {
@@ -6,28 +7,27 @@ public:
         int left = 0, n = forts.size();
 
         while (left < n) {
-            while (left < n && forts[left] == 0) left++;
+            while (left < n && forts[left] == 0)
+                left++;
 
             int right = left + 1;
 
-
-            while (right < n && forts[right] == 0) right++;
+            while (right < n && forts[right] == 0)
+                right++;
             if (right < n && forts[left] + forts[right] == 0) {
                 bool valid_path = true;
-                for(int i = left+1;i<right;i++){
-                    if(forts[i] != 0){
+                for (int i = left + 1; i < right; i++) {
+                    if (forts[i] != 0) {
                         valid_path = false;
                         break;
                     }
-                   
-                    
                 }
-                if(valid_path){
-                maxDistance = max(maxDistance, right - left - 1);
+                if (valid_path) {
+                    maxDistance = max(maxDistance, right - left - 1);
                 }
             }
 
-            left = right; 
+            left = right;
         }
 
         return maxDistance;
