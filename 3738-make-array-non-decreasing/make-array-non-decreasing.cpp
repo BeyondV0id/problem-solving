46b20c8d7342bcdd7565f6396ccb43f055910c7b
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-20 13:48:46 +0530

    Time: 40 ms (7.64%) | Memory: 211.8 MB (12.50%) - LeetSync

--- 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
         //monotonic increasinng stack
-        int n = nums.size();
-        int cnt = 1;
-        int prev = nums[0];
+        
+        stack<int> s;
 
-        for (int i=1;i<n;i++) {
-            if(nums[i]>=prev){
-                cnt++;
-                prev = nums[i];
-                
+        for (int num : nums) {
+            while (!s.empty() && s.top() > num) {
+                int top = s.top();
+                s.pop();
+                num = top;
             }
+            s.push(num);
         }
 
-       return cnt;
+        return s.size();
     }
 };
