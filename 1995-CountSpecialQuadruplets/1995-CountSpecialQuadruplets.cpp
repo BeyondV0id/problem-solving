fd197d12b6c3d4b78eb65096eb5d784ba358bdff
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-22 04:04:24 +0530

    Create file

---
+++ 1995-CountSpecialQuadruplets/1995-CountSpecialQuadruplets.cpp
@@ -0,0 +1,26 @@
+// Last updated: 4/22/2025, 4:04:24 AM
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) {
+    int n = nums.size();
+    int cnt = 0;
+    for (int c = 2; c < n - 1; c++) {
+        unordered_map<int, int> sumcnt;
+        for (int a = 0; a < c; a++) {
+            for (int b = a + 1; b < c; b++) {
+                int sum = nums[a] + nums[b];
+                sumcnt[sum]++;
+            }
+        }
+        for (int d = c + 1; d < n; d++) {
+            int diff = nums[d] - nums[c];
+            if (sumcnt.count(diff)) {
+                cnt += sumcnt[diff];
+            }
+        }
+    }
+
+    return cnt;
+}
+
+};
