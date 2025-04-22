1fd62ceaea93246b6b86258dd7320e8a070079c3
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-22 17:54:16 +0530

    Create file

---
+++ 781-RabbitsinForest/781-RabbitsinForest.cpp
@@ -0,0 +1,29 @@
+// Last updated: 4/22/2025, 5:54:16 PM
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        int n = answers.size();
+        int cnt = 0;
+        unordered_map<int, int> freq;
+        unordered_set<int> unique;
+        for (int num : answers) {
+            freq[num]++;
+            unique.insert(num);
+        }
+        for (int num : unique) {
+            if(freq[num] == 0)continue;
+            int groupsize = num + 1;
+            int rabbits = freq[num];
+            if (rabbits <= num) {
+                cnt += groupsize;
+            }
+            else{
+                int grps = (rabbits+groupsize-1)/groupsize;
+                cnt+=grps*groupsize;
+
+                
+            }
+        }
+        return cnt;
+    }
+};
