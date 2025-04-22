92c3c15603d05ccee5e09566960b197528903d77
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-22 19:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (100.00%) - LeetSync

---
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        /* for(int i=0;i<n;i++){
+             cout<<(2 * i) + 1<<" ";*/
+        int cnt = 0;
+        int target = n;
+        for (int i = 0; i < n / 2; i++) {
+            cnt += target - (2 * i + 1);
+        }
+        return cnt;
+    }
+}
+
+;
