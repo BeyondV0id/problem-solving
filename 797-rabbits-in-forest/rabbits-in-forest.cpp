5423bc4f75462c7a54340ba202baea8083f51a9d
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-22 17:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (11.85%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -10,17 +10,15 @@ public:
             unique.insert(num);
         }
         for (int num : unique) {
-            if(freq[num] == 0)continue;
+            if (freq[num] == 0)
+                continue;
             int groupsize = num + 1;
             int rabbits = freq[num];
             if (rabbits <= num) {
                 cnt += groupsize;
-            }
-            else{
-                int grps = (rabbits+groupsize-1)/groupsize;
-                cnt+=grps*groupsize;
-
-                
+            } else {
+                int grps = (rabbits + groupsize - 1) / groupsize;
+                cnt += grps * groupsize;
             }
         }
         return cnt;
