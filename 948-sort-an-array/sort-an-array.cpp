95f5851eff3d49d4c4b570ef607bb2c20eb89e9e
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-23 06:32:13 +0530

    Time: 203 ms (55.47%) | Memory: 146.6 MB (42.67%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+
+private:
+    void mergeSort(vector<int>& nums, int low, int high) {
+        if (low >= high) return;
+        int mid = low + (high - low) / 2;
+
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid + 1, high);
+        outPlaceMerge(nums, low, mid, high);
+    }
+
+    void outPlaceMerge(vector<int>& nums, int low, int mid, int high) {
+        if (low >= high) return;
+
+        int l = low, r = mid + 1, k = 0;
+        int size = high - low + 1;
+        vector<int> sorted(size, 0);
+
+        while (l <= mid && r <= high)
+            sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
+        
+        while (l <= mid)
+            sorted[k++] = nums[l++];
+        
+        while (r <= high)
+            sorted[k++] = nums[r++];
+        
+        for (k = 0; k < size; k++)
+            nums[low + k] = sorted[k];
+    }
+};
