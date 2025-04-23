93c389b932a85bf79d7f3e0524425071bbe1c734
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-23 09:19:15 +0530

    Create file

---
+++ 1399-CountLargestGroup/1399-CountLargestGroup.cpp
@@ -0,0 +1,34 @@
+// Last updated: 4/23/2025, 9:19:14 AM
+class Solution {
+private:
+    int find_digit_Sum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }
+
+public:
+    int countLargestGroup(int n) {
+        unordered_map<int, int> grps;
+        for (int i = 1; i <= n; i++) {
+            int sum = find_digit_Sum(i);
+            grps[sum]++;
+        }
+        int maxVal = 1;
+        for (auto num : grps) {
+            if (num.second > maxVal) {
+                maxVal = num.second;
+            }
+        }
+        int cnt = 0;
+        for (auto num : grps) {
+            if (num.second == maxVal) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
