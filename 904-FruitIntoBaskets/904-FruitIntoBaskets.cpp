444ff0c74d8795dd56c453734e2759a818e817f3
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-24 05:11:15 +0530

    File updated at 4/24/2025, 5:11:14 AM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.cpp
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.cpp
@@ -1,21 +1,20 @@
-// Last updated: 4/24/2025, 5:10:46 AM
+// Last updated: 4/24/2025, 5:11:14 AM
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int size = 0;
-        int l = 0;
-        unordered_map<int,int> seen;
-        int maxLen = 0;
-
-        for (int r = 0; r < fruits.size(); r++) {
-            seen[fruits[r]]++;
-            while (seen.size() > 2) {
-                seen[fruits[l]]--;
-                if(seen[fruits[l]] == 0)
-                    seen.erase(fruits[l]);
+        int n=fruits.size();
+        int l=0,r=0,maxLen=0;
+        unordered_map<int,int> mp;
+        while(r<n) {
+            mp[fruits[r]]++;
+            while(mp.size()>2) {
+                mp[fruits[l]]--;
+                if(mp[fruits[l]]==0) mp.erase(fruits[l]);
                 l++;
             }
-            maxLen = max(maxLen, (r-l)+1);
+            int len=r-l+1;
+            maxLen=max(len,maxLen);
+            r++;
         }
         return maxLen;
     }
