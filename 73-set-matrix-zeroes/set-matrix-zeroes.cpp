3cbea02548074c93f047b48890ff0c17c00635f0
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-26 16:15:57 +0530

    Time: 2 ms (13.75%) | Memory: 18.7 MB (24.59%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<pair<int, int>> zerolocs;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] == 0)
+                    zerolocs.push_back({i, j});
+
+        for (auto& z : zerolocs) {
+            int x = z.first;
+            int y = z.second;
+            for (int i = 0; i < m; i++)
+                matrix[i][y] = 0;
+            for (int j = 0; j < n; j++)
+                matrix[x][j] = 0;
+        }
+    }
+};
+// @lc code=end
