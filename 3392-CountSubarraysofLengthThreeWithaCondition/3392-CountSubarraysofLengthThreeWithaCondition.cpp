d23fb00ba92e8bc97940c9b6ac5c4519c8dd8ef6
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-27 06:35:23 +0530

    Create file

---
+++ 3392-CountSubarraysofLengthThreeWithaCondition/3392-CountSubarraysofLengthThreeWithaCondition.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/27/2025, 6:35:21 AM
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int size = 3;
+        int l = 0, r;
+        int cnt = 0;
+
+        for (r = 0; r < nums.size(); r++) {
+
+            int subsize = (r - l + 1);
+
+            if (subsize == size) {
+                int mid = l+(r-l)/2;
+                if (2*(nums[l] + nums[r])== nums[mid])
+                    cnt++;
+                l++;
+            }
+        }
+        return cnt;
+    }
+};
