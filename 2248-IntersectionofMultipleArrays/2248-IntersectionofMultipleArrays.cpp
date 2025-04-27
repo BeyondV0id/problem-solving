6bf7e19b59db81249bf60587241fcd98ab6d575d
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-27 06:38:02 +0530

    Create file

---
+++ 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/27/2025, 6:38:00 AM
+class Solution {
+public:
+    vector<int> intersection(vector<vector<int>>& nums) {
+        vector<int> result;
+        int m = nums.size();
+        unordered_map<int, int> seen;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < nums[i].size(); j++)
+                seen[nums[i][j]]++;
+        for (auto num : seen) {
+            if (num.second == m)
+                result.push_back(num.first);
+        }
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
+// @lc code=end
