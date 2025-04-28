8869d8b2d19ea9b02c4fc7cb5c5ea35442de9e35
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-28 17:10:21 +0530

    Create file

---
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
@@ -0,0 +1,50 @@
+// Last updated: 4/28/2025, 5:10:19 PM
+class Solution {
+private:
+    stack<string> strbuild(string s) {
+        stack<string> stk;
+        if (s.empty())
+            return stk;
+
+        int n = s.size();
+        int l = 0, r = 0;
+        while (r < n) {
+            if (s[r] != ' ') {
+                r++;
+                continue;
+            }
+
+            if (l != r) {
+                string c = s.substr(l, r - l);
+                stk.push(c);
+            }
+            do {
+                r++;
+            } while (r < n && s[r] == ' ');
+            l = r;
+        }
+
+        if (l != r) {
+            stk.push(s.substr(l, r - l));
+        }
+
+        return stk;
+    }
+
+    string stackTostr(stack<string>& stk) {
+        string result;
+        while (!stk.empty()) {
+            result += stk.top() + " ";
+            stk.pop();
+        }
+        if (!result.empty())
+            result.pop_back();
+        return result;
+    }
+
+public:
+    string reverseWords(string s) {
+        stack<string> builder = strbuild(s);
+        return stackTostr(builder);
+    }
+};
