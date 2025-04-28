56593196fd185e89cecd816d8e90cdbf2c1c40ca
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-28 08:41:31 +0530

    Time: 1340 ms (41.24%) | Memory: 517.3 MB (27.84%) - LeetSync

--- 3707-find-the-most-common-response/find-the-most-common-response.cpp
+++ 3707-find-the-most-common-response/find-the-most-common-response.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string findCommonResponse(vector<vector<string>>& responses) {
-        bool sameArray = true;
         int n = responses.size();
 
         unordered_map<string, int> freq;
