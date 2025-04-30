ffc02c7d65f2f767e5ec9201091ce17ee23fb697
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-04-30 13:54:31 +0530

    File updated at 4/30/2025, 1:54:30 PM

--- 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.cpp
+++ 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.cpp
@@ -1,32 +1,38 @@
-// Last updated: 4/30/2025, 1:53:55 PM
+// Last updated: 4/30/2025, 1:54:30 PM
 class Solution {
-private:
-    int sumDigits(int a) {
-        int sum = 0;
-        while (a > 0) {
-            sum += a % 10;
-            a = a / 10;
+public:
+    static inline int digitSum(int x){
+        int sum=0;
+        for(; x>0; x/=10){
+            sum+=x%10;
         }
         return sum;
     }
-
-public:
-    int maximumSum(vector<int>& nums) {
-        int n = nums.size();
-        int sum = -1;
-        unordered_map<int, int> sumMap;
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int digitSum = sumDigits(num);
-
-            if (sumMap.count(digitSum)) {
-                int newSum = num + sumMap[digitSum];
-                sum = max(sum, newSum);
-                sumMap[digitSum] = max(sumMap[digitSum], num);
-            } else {
-                sumMap[digitSum] = num;
-            }
+    static int maximumSum(vector<int>& nums) {
+        int p[82][2]={{0, 0}};
+    //    memset(pq, 0, sizeof(pq));
+        int maxD=0, minD=INT_MAX;
+        for(int x: nums){
+            int D=digitSum(x);
+            if (p[D][0]==0 && p[D][1]==0) p[D][1]=x;
+            else if (x>=p[D][1]) p[D][0]=exchange(p[D][1], x);
+            else if (x>=p[D][0]) p[D][0]=x;
+            maxD=max(maxD, D);
+            minD=min(minD, D);
         }
-        return sum;
+        
+        int ans=-1;
+        for(int D=minD; D<=maxD; D++){
+            if (p[D][0]==0) continue;
+            ans=max(ans, p[D][0]+p[D][1]);
+        }
+        return ans;
     }
 };
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
