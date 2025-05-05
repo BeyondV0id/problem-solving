beb6f2eb71af7ef1bf01c50c389bbbdf5bf2c654
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-05-05 21:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (81.52%) - LeetSync

---
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        bool seenB = false;
+
+        for (auto& c : s) {
+            if (c == 'b')
+                seenB = true;
+            if (c == 'a' && seenB)
+                return false;
+        }
+        return true;
+    }
+};
