8184bdec1e089afb4f141e9d1f139687d9adbdd5
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-05-08 10:28:34 +0530

    Time: 2141 ms (5.02%) | Memory: 685.4 MB (7.53%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,67 +1,49 @@
 class Solution {
 private:
     void merge(vector<vector<int>>& intervals, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
+        int i = left, j = mid + 1;
+        vector<vector<int>> temp;
 
-        vector<vector<int>> la(n1), ra(n2);
-
-        for (int i = 0; i < n1; i++)
-            la[i] = intervals[left + i];
-        for (int j = 0; j < n2; j++)
-            ra[j] = intervals[mid + 1 + j];
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            if (la[i][1] <= ra[j][1]) {
-                intervals[k] = la[i];
-                i++;
+        while (i <= mid && j <= right) {
+            if (intervals[i][1] <= intervals[j][1]) {
+                temp.push_back(intervals[i++]);
             } else {
-                intervals[k] = ra[j];
-                j++;
+                temp.push_back(intervals[j++]);
             }
-            k++;
         }
 
-        while (i < n1) {
-            intervals[k] = la[i];
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            intervals[k] = ra[j];
-            j++;
-            k++;
+        while (i <= mid) temp.push_back(intervals[i++]);
+        while (j <= right) temp.push_back(intervals[j++]);
+
+        for (int k = 0; k < temp.size(); ++k) {
+            intervals[left + k] = temp[k];
         }
     }
 
     void mergeSort(vector<vector<int>>& intervals, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            mergeSort(intervals, left, mid);
-            mergeSort(intervals, mid + 1, right);
-            merge(intervals, left, mid, right);
-        }
+        if (left >= right) return;
+        int mid = left + (right - left) / 2;
+        mergeSort(intervals, left, mid);
+        mergeSort(intervals, mid + 1, right);
+        merge(intervals, left, mid, right);
     }
 
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if (intervals.empty()) return 0;
 
-
         mergeSort(intervals, 0, intervals.size() - 1);
 
-        int cnt = 1;  
+        int count = 1;
         vector<int> prev = intervals[0];
 
-        for (int i = 1; i < intervals.size(); i++) {
-            vector<int> cur = intervals[i];
-            if (prev[1] <= cur[0]) {  
-                cnt++;
-                prev = cur;
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (prev[1] <= intervals[i][0]) {
+                count++;
+                prev = intervals[i];
             }
         }
 
-        return intervals.size() - cnt;
+        return intervals.size() - count;
     }
 };
-
