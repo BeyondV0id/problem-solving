5e858b198e7adce89a2544d56d5b0bafa024a342
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-05-10 07:45:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (49.91%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        int topWall = 0;
+        int bottomWall = matrix.size() - 1;
+        int rightWall = matrix[0].size() - 1;
+        int leftWall = 0;
+        int direction = 0;
+
+        while (topWall <= bottomWall && leftWall <= rightWall) {
+            if (direction == 0) {
+                for (int i = leftWall; i <= rightWall; i++) {
+                    res.push_back(matrix[topWall][i]);
+                }
+                topWall++;
+            } else if (direction == 1) {
+                for (int i = topWall; i <= bottomWall; i++) {
+                    res.push_back(matrix[i][rightWall]);
+                }
+                rightWall--;
+            } else if (direction == 2) {
+                for (int i = rightWall; i >= leftWall; i--) {
+                    res.push_back(matrix[bottomWall][i]);
+                }
+                bottomWall--;
+            } else if (direction == 3) {
+                for (int i = bottomWall; i >= topWall; i--) {
+                    res.push_back(matrix[i][leftWall]);
+                }
+             leftWall++;
+            }
+            direction = (direction + 1) % 4;
+        }
+        return res;
+    }
+};
+// @lc code=end
