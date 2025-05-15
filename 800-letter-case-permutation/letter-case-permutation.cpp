d0af7ca635ff1c043f1be83768a5f149420216ae
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-05-15 09:40:39 +0530

    Time: 3 ms (61.83%) | Memory: 13.8 MB (73.64%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    vector<string> res;
+    void solve(string& s, int indx) {
+        if (s.size() == indx) {
+            res.push_back(s);
+            return;
+        }
+        if (isdigit(s[indx])) {
+            solve(s, indx + 1);
+        } else {
+            //first branch
+            solve(s, indx + 1);
+            if (islower(s[indx]))
+                s[indx] = toupper(s[indx]);
+            else
+                s[indx] = tolower(s[indx]);
+            //second
+            solve(s, indx + 1);
+            if (islower(s[indx]))
+               s[indx] =  toupper(s[indx]);
+            else
+               s[indx] =  tolower(s[indx]);
+        }
+    }
+
+public:
+    vector<string> letterCasePermutation(string s) {
+        solve(s, 0);
+        return res;
+    }
+};
+
