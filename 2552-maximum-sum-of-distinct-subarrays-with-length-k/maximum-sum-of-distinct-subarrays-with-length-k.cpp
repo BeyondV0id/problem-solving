aba74d9af48fa48b381697df9d5d9adde7f967ef
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-05-16 16:44:18 +0530

    Time: 127 ms (20.01%) | Memory: 99.7 MB (29.30%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,33 +1,33 @@
 class Solution {
+private:
+    int l = 0;
+    int r = 0;
+    unordered_set<int> flag;
+    long long sum = 0;
+    long long maxxi = 0;
+
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_set<int> flag;
-        long long sum = 0;
-        long long maxxi = 0;
-        int l = 0;
-        
-        for (int r = 0; r < n; r++) {
-            if (!flag.count(nums[r])) {
-                sum += nums[r];
-                flag.insert(nums[r]);
-                
-                if (r - l + 1 == k) {
-                    maxxi = max(maxxi, sum);
-                    sum -= nums[l];
-                    flag.erase(nums[l]);
-                    l++;
-                }
-            } else {
-                while (nums[l] != nums[r]) {
-                    sum -= nums[l];
-                    flag.erase(nums[l]);
-                    l++;
-                }
+        if(n<k)
+            return 0;
+        for (r = 0; r < n; r++) {
+            while (flag.count(nums[r])) {
+                flag.erase(nums[l]);
+                sum -= nums[l];
+                l++;
+            }
+            sum += nums[r];
+            flag.insert(nums[r]);
+            if (r-l+1 == k) {
+                maxxi = max(sum, maxxi);
+                flag.erase(nums[l]);
+                sum-=nums[l];
                 l++;
             }
         }
-        
+
         return maxxi;
     }
 };
+// @lc code=end
