09b58030a3b49e6c28366f32a07dcef73d8593aa
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-06-17 12:07:24 +0530

    Time: 3 ms (21.95%) | Memory: 55.6 MB (24.70%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=22 lang=javascript
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let result = [],subSol = [];
+    function backtrack(openCnt,closeCnt,subSol){
+        if(subSol.length === 2*n){
+            result.push(subSol.join(''));
+            return;
+        }
+        if(openCnt < n){
+            subSol.push('(');
+            backtrack(openCnt+1,closeCnt,subSol);
+            subSol.pop();
+        }
+        if(closeCnt < openCnt){
+            subSol.push(')');
+            backtrack(openCnt,closeCnt+1,subSol);
+            subSol.pop();
+        }
+
+        
+    }
+    backtrack(0,0,subSol);
+    return result;
+};
+// @lc code=end
+
