66d7f0b772d7f6a268457ee5ae768a471707e36c
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-07-02 10:05:40 +0530

    Time: 17 ms (69.07%) | Memory: 57.3 MB (44.73%) - LeetSync

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1456 lang=javascript
+ *
+ * [1456] Maximum Number of Vowels in a Substring of Given Length
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function(s, k) {
+    let cnt = 0,maxCnt = -Infinity;
+    const n = s.length;
+    let l=0,r=0;
+    function isVowel(char){
+        return 'aeiouAEIOU'.includes(char);
+    }
+    while(r<n){
+        const char = s[r++];
+        if(isVowel(char))
+            cnt++;
+        while(r-l > k){
+            if(isVowel(s[l]))
+                cnt--;
+            l++;
+        }
+        maxCnt = Math.max(maxCnt,cnt);
+
+    }
+    return maxCnt;  
+};
+// @lc code=end
+
