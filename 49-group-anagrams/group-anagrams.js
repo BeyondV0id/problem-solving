48d4dbef1a392d9a348b4bd4e06f760a2d210223
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-07-04 10:25:14 +0530

    Time: 27 ms (80.93%) | Memory: 68.9 MB (26.54%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=49 lang=javascript
+ *
+ * [49] Group Anagrams
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = {};
+    for(let word of strs){
+        const key = word.split('').sort().join('');
+        if(!map[key])
+            map[key] = [];
+        map[key].push(word);
+    }
+    const result = Object.values(map);
+    return result;
+
+
+
+    
+};
+// @lc code=end
+
