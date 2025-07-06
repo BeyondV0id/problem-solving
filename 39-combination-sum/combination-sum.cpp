9bec4e293c577b87c413564c0dec15918063e7b7
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-07-06 10:16:58 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (46.66%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,29 +1,28 @@
-#include<vector>
+#include <vector>
 using namespace std;
+
 class Solution {
 private:
-    vector<vector<int>>result;
-    vector<int>array;
-    void backtrack(vector<int>& candidates, int target,int indx,int total){
-        if(total == target){
+    vector<vector<int>> result;
+    vector<int> array;
+
+    void backtrack(vector<int>& candidates, int target, int start, int sum) {
+        if (sum == target) {
             result.push_back(array);
             return;
         }
-        if(indx >= candidates.size() || target < total)
-            return;
-        const int num = candidates[indx];
-        array.push_back(num);
-        backtrack(candidates,target,indx,total+num);
-        array.pop_back();
-        backtrack(candidates,target,indx+1,total);
+        if (sum > target) return;
+
+        for (int i = start; i < candidates.size(); i++) {
+            array.push_back(candidates[i]);                     
+            backtrack(candidates, target, i, sum + candidates[i]); 
+            array.pop_back();                                 
+        }
     }
+
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        backtrack(candidates,target,0,0);
+        backtrack(candidates, target, 0, 0);
         return result;
-
-        
     }
 };
-// @lc code=end
-
