075785b47077b6912525c74da08df07f1ac5f769
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-07-09 11:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (43.65%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/battleships-in-a-board.cpp
@@ -1,10 +1,43 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+    void dfs(vector<vector<char>>& board, int r, int c, int rows, int cols) {
+        if (r < 0 || r >= rows || c < 0 || c >= cols || board[r][c] != 'X')
+            return;
+        board[r][c] = '.';
+        dfs(board, r + 1, c, rows, cols);
+        dfs(board, r, c + 1, rows, cols);
+    }
+
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+        int count = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (board[r][c] == 'X') {
+                    dfs(board, r, c, rows, cols);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+/*
+BFS version:
+
 #include <queue>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
-//bfs
     int countBattleships(vector<vector<char>>& board) {
         int rows = board.size();
         int cols = board[0].size();
@@ -42,31 +75,4 @@ public:
         return count;
     }
 };
-
-/*dfs
-private:
-    int cnt = 0;
-    void dfs(vector<vector<char>>& board, int r, int c, int rows, int cols) {
-        if (r < 0 || r >= rows || c < 0 || c >= cols)
-            return;
-        if (board[r][c] != 'X')
-            return;
-        board[r][c] = '.';
-        dfs(board, r + 1, c, rows, cols);
-        dfs(board, r, c + 1, rows, cols);
-    }
-
-public:
-    int countBattleships(vector<vector<char>>& board) {
-        const int rows = board.size();
-        const int cols = board[0].size();
-        for (int r = 0; r < rows; r++)
-            for (int c = 0; c < cols; c++)
-                if (board[r][c] == 'X') {
-                    dfs(board, r, c, rows, cols);
-                    cnt++;
-                }
-        return cnt;
-    }
 */
-
