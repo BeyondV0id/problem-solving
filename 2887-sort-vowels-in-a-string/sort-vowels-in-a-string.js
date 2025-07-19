0d8aab3c9bf8b19b768d75a0aeefac4b51ba549a
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-07-19 10:32:32 +0530

    Time: 74 ms (54.17%) | Memory: 77.8 MB (10.42%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.js
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.js
@@ -10,28 +10,24 @@
  * @return {string}
  */
 var sortVowels = function(s) {
-    let t = [];
-    let vowels = [];
-    let isVowel = (char)=>{
-        return  'aeiouAEIOU'.includes(char);
-    }
-    for(const char of s){
-        if(isVowel(char)){
-            vowels.push(char);
+    const vowels = new Set(
+        ['a','e','i','o','u','A','E','I','O','U']
+     );
+   const strArr = s.split('');
+   const vowelIndxs = [];
+   const vowelArr = [];
+
+   for(let i=0;i<strArr.length;i++)
+        if(vowels.has(strArr[i])){
+            vowelArr.push(strArr[i]);
+            vowelIndxs.push(i);
         }
+    vowelArr.sort();
+    for(let i=0;i<vowelIndxs.length;i++){
+        strArr[vowelIndxs[i]] = vowelArr[i];
     }
-    vowels.sort();
-    let i = 0;
-    for(const char of s){
-        if(!isVowel(char)){
-            t.push(char);
-        }
-        else{
-            t.push(vowels[i++]);
-        }
-    }
-    return t.join('');
-    
+    return strArr.join('');
+   
 };
 // @lc code=end
 
