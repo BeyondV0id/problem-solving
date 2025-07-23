ed8f3f4a93974e5604443273441ab77a085fd3a7
Harsha Vardhan Gandepalli <149029854+harsha007009@users.noreply.github.com>
2025-07-23 17:53:34 +0530

    Time: 47 ms (40.91%) | Memory: 90.1 MB (49.17%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=2033 lang=cpp
+ *
+ * [2033] Minimum Operations to Make a Uni-Value Grid
+ */
+
+// @lc code=start
+#include <algorithm>
+#include <vector>
+using namespace std;
+class Solution {
+private:
+    vector<int> arr;
+
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        for (auto& row : grid)
+            for (auto& val : row)
+                arr.push_back(val);
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        int target = arr[n / 2];
+        int cnt = 0;
+        for (auto& row : grid)
+            for (auto& val : row) {
+                if (abs(target - val) % x == 0)
+                    cnt += abs((target - val) / x);
+                else
+                    return -1;
+            }
+        return cnt;
+    }
+};
+// @lc code=end
