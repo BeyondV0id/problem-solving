0d64fbdffd603d7b11f1fc405d986014682aad46
Harsha Vardhan <149029854+harsha007009@users.noreply.github.com>
2025-09-05 10:53:32 +0530

    Time: 762 ms (18.84%) | Memory: 11.6 MB (83.15%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -6,22 +6,21 @@ using namespace std;
 // @leet start
 class Solution {
 private:
-  int res = 0;
-  void backtrack(vector<int>& nums, int target,int index,int curr){
-    if(index == (int)nums.size()){
-      if(curr == target)
-        res += 1;
-      return;
+    int res = 0;
+    void backtrack(vector<int>& nums, int target, int index, int curr) {
+        if (index == (int)nums.size()) {
+            if (curr == target)
+                res += 1;
+            return;
+        }
+        backtrack(nums, target, index + 1, curr + nums[index]);
+        backtrack(nums, target, index + 1, curr - nums[index]);
     }
-    backtrack(nums,target,index+1,curr+nums[index]);
-    backtrack(nums,target,index+1,curr-nums[index]);
 
-  }
 public:
-  int findTargetSumWays(vector<int>& nums, int target) {
-    backtrack(nums,target,0,0);
-    return res;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return res;
     }
 };
 // @leet end
-
