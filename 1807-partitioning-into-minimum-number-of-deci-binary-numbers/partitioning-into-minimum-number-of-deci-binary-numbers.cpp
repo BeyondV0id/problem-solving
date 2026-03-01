4e1254f225ffcc80f6ced0bf50ca266b76163f34
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-03-01 11:29:43 +0530

    Time: 4 ms (28.60%) | Memory: 16.6 MB (53.96%) - LeetSync

---
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,17 @@
+// @leet start
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxDigit = 0;
+
+        for (char c : n) {
+            maxDigit = max(maxDigit, c - '0');
+        
+            if (maxDigit == 9)
+                return 9;
+        }
+
+        return maxDigit;
+    }
+};
+// @leet end
