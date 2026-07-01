e1e149e9f95b34ff37252f46af40d6bb531b97f8
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-07-01 15:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (19.35%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,53 @@
 class Solution {
+
+    int lowerBound(vector<int>&nums,int target){
+        int l = 0;
+        int r = nums.size()-1;
+        int ans = -1;
+
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+
+            //go left
+
+            if(target <= nums[mid]){
+                if(target == nums[mid])ans = mid;
+                r = mid-1;
+            }
+            else if(target > nums[mid]){
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+        int upperBound(vector<int>&nums,int target){
+        int l = 0;
+        int r = nums.size()-1;
+        int ans = -1;
+
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+
+            //go right
+            if(target >= nums[mid]){
+                if(target == nums[mid])ans = mid;
+                l = mid + 1;
+            }
+            else if(target < nums[mid]){
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result(2, -1); // Initialize with {-1, -1}
-        
-        int l = 0, r = nums.size() - 1;
-        int firstPos = -1;
-        
-        // Finding the first occurrence (lower bound)
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (nums[mid] >= target) {
-                if (nums[mid] == target) firstPos = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-
-        int lastPos = -1;
-        l = 0, r = nums.size() - 1;
-
-        // Finding the last occurrence (upper bound) 
-        //but different as we return the index of number we use  <=. 
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (nums[mid] > target) {
-                r = mid - 1;
-            } else {
-                if(nums[mid] == target)lastPos = mid;
-                l = mid + 1;
-            }
-        }
-
-        result[0] = firstPos;
-        result[1] = lastPos;
-        return result;
+        vector<int>res;
+        int lb = lowerBound(nums,target);
+        int ub = upperBound(nums,target);
+        res.push_back(lb);
+        res.push_back(ub);
+        return res;
     }
 };
