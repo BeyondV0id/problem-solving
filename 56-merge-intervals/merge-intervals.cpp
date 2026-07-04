09d21320944350018c6571086f5c64b1c5eaecdd
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-07-04 17:06:57 +0530

    Time: 6 ms (44.89%) | Memory: 24.2 MB (19.69%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,32 +1,29 @@
-// @leet imports start
-#include <bits/stdc++.h>
-using namespace std;
-// @leet imports end
-
-// @leet start
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> result;
-        sort(intervals.begin(), intervals.end());
+
+        int n = intervals.size();
+
+
+        vector<vector<int>>result;
+
+
+        sort(intervals.begin(),intervals.end());
+
         result.push_back(intervals[0]);
-        pair<int, int> prev = {intervals[0][0], intervals[0][1]};
 
-        for (int i = 1; i < (int)intervals.size(); i++) {
-            int begin = intervals[i][0];
-            int end = intervals[i][1];
 
-            if (prev.second >= begin) {
-                int longer = max(end, prev.second);
+        for(int i = 1; i < n; i++){
+            if(intervals[i][0] <= result.back()[1]){
+                int right = max(result.back()[1],intervals[i][1]);
+                int left = result.back()[0];
                 result.pop_back();
-                result.push_back({prev.first, longer});
-                prev = {prev.first, longer};
-            } else {
+                result.push_back({left,right});
+            }
+            else{
                 result.push_back(intervals[i]);
-                prev = {intervals[i][0], intervals[i][1]};
             }
         }
         return result;
     }
 };
-// @leet end
