e3f416847c5ddd8e0484aa38bbd75f400d205f28
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-07-28 15:26:53 +0530

    Time: 18 ms (97.95%) | Memory: 16.3 MB (70.54%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,47 +1,39 @@
-/*
- * @lc app=leetcode id=200 lang=cpp
- *
- * [200] Number of Islands
- */
-
-// @lc code=start
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 #include <vector>
 using namespace std;
 class Solution {
 private:
-    int cnt = 0;
-    vector<pair<int, int>> directions = {
-        {1, 0},
-        {0, 1},
-        {-1, 0},
-        {0, -1},
-    };
-    void dfs(vector<vector<char>>& grid, const int rows, const int cols, int r,
-             int c) {
-        if (r >= rows || c >= cols || r < 0 || c < 0)
-            return;
-        if (grid[r][c] == '0')
-            return;
-        grid[r][c] = '0';
-        for (auto [dr, dc] : directions) {
-            dfs(grid, rows, cols, r + dr, c + dc);
+    vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {1, 0}, {0, 1}};
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        grid[i][j] = '0';
+
+        for (auto [dx, dy] : dirs) {
+            int x = dx + i;
+            int y = dy + j;
+            if (x >= m || y >= n || x < 0 || y < 0)
+                continue;
+            if (grid[x][y] == '1')
+                dfs(grid, x, y);
         }
     }
 
 public:
     int numIslands(vector<vector<char>>& grid) {
-        const int rows = grid.size();
-        const int cols = grid[0].size();
-        for (int r = 0; r < rows; r++)
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1') {
-                    dfs(grid, rows, cols, r, c);
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
                     cnt++;
                 }
             }
+        }
         return cnt;
     }
 };
-// @lc code=end
