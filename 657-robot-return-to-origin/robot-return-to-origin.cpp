9719345fc4e4e446b1cf708a9e01a5a6cb925e0e
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-07-29 20:12:01 +0530

    Time: 8 ms (9.26%) | Memory: 10.3 MB (77.44%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        unordered_map<char, int> dirs = {
-            {'L', -1}, {'R', 1}, {'U', -2}, {'D', 2}};
-
-        int x = 0;
-        int y = 0;
-        for (auto ch : moves) {
-            int dir = dirs[ch];
-            if(dir == -1 || dir == 1) x += dir;
-            else y += dir;
+        int x=0;
+        int y=0;
+        for(char ch : moves){
+            if(ch == 'R')
+            x++;
+            else if(ch == 'L')
+            x--;
+            else if(ch == 'U')
+            y++;
+            else if(ch == 'D')
+            y--;
         }
-
-        if(x == 0 && y == 0)return true;
-
-        return false;
+        return x==0 && y==0;
     }
 };
