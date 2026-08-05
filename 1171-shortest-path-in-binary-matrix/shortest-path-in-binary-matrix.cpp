57946f409758ad8bcde2a442871757fa59990194
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-08-05 14:21:35 +0530

    Time: 8 ms (94.70%) | Memory: 23.6 MB (80.29%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,49 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-using namespace std;
-
 class Solution {
 private:
-    int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-    int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-    queue<int> rq, cq, cntq;
-
-    void explore_neighbours(int r, int c, int cnt, vector<vector<int>>& grid, int n) {
-        for (int d = 0; d < 8; d++) {
-            int rr = r + dr[d];
-            int cc = c + dc[d];
-            if (rr >= 0 && rr < n && cc >= 0 && cc < n && grid[rr][cc] == 0) {
-                rq.push(rr);
-                cq.push(cc);
-                cntq.push(cnt + 1);
-                grid[rr][cc] = 1;
-            }
-        }
-    }
+    vector<pair<int, int>> dirs = {{0, 1},   {1, 0}, {-1, 0}, {0, -1},
+                                   {-1, -1}, {1, 1}, {1, -1}, {-1, 1}};
 
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0)
+
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
             return -1;
 
-        grid[0][0] = 1; // mark visited
-        rq.push(0);
-        cq.push(0);
-        cntq.push(1);
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        grid[0][0] = 1;
 
-        while (!rq.empty()) {
-            int r = rq.front(); rq.pop();
-            int c = cq.front(); cq.pop();
-            int cnt = cntq.front(); cntq.pop();
+        int len = 1;
 
-            if (r == n - 1 && c == n - 1)
-                return cnt;
+        while (!q.empty()) {
+            int sz = q.size();
 
-            explore_neighbours(r, c, cnt, grid, n);
+            while (sz--) {
+                auto [x, y] = q.front();
+                q.pop();
+
+                if (x == n - 1 && y == n - 1)
+                    return len;
+
+                for (auto [dx, dy] : dirs) {
+                    int nx = x + dx;
+                    int ny = y + dy;
+
+                    if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                        continue;
+
+                    if (grid[nx][ny] == 1)
+                        continue;
+
+                    grid[nx][ny] = 1;
+                    q.push({nx, ny});
+                }
+            }
+
+            len++;
         }
+
         return -1;
     }
 };
