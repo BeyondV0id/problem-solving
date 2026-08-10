c5ebbd9d1661f91a7e383c3abc85db341a27e2cf
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-08-10 16:45:09 +0530

    Time: 24 ms (44.68%) | Memory: 29.1 MB (62.27%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int m = moveTime.size();
+        int n = moveTime[0].size();
+
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+
+        vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+        dist[0][0] = 0;
+        pq.push({0, {0, 0}});
+
+        while (!pq.empty()) {
+            auto [time, pos] = pq.top();
+            pq.pop();
+
+            int x = pos.first;
+            int y = pos.second;
+
+            if (time > dist[x][y])
+                continue;
+
+            if (x == m - 1 && y == n - 1)
+                return time;
+
+            for (auto [dx, dy] : dirs) {
+                int nx = x + dx;
+                int ny = y + dy;
+
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                    int newTime = max(time, moveTime[nx][ny]) + 1;
+
+                    if (newTime < dist[nx][ny]) {
+                        dist[nx][ny] = newTime;
+                        pq.push({newTime, {nx, ny}});
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
