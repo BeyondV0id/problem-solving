17b0f85902dbe45c64c08247418e6276a454c4df
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-08-13 16:06:55 +0530

    Time: 35 ms (54.04%) | Memory: 18.9 MB (62.32%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -6,29 +6,33 @@ using namespace std;
 
 // @leet start
 class Solution {
-  private:
-    int solve(vector<int> &coins, int amount, vector<int> &dp) {
+private:
+    int solve(vector<int>& coins, int amount, vector<int>& dp) {
 
-      if(amount == 0)return 0;
-      if(amount < 0)return 1e9;
-      int n = coins.size();
-      if(dp[amount] != -1)return dp[amount];
+        if (amount == 0)
+            return 0;
+        if (amount < 0)
+            return 1e9;
+        int n = coins.size();
 
-      int mini = 1e9;
-      for(int i = 0; i < n; i++){
-        if(coins[i] <= amount){
-          mini = min(mini,1 + solve(coins,amount-coins[i],dp));
+        if (dp[amount] != -1)
+            return dp[amount];
+
+        int mini = 1e9;
+        for (int i = 0; i < n; i++) {
+            if (coins[i] <= amount) {
+                mini = min(mini, 1 + solve(coins, amount - coins[i], dp));
+            }
         }
-      }
-      return dp[amount] = mini;
+        return dp[amount] = mini;
     }
 
-  public:
-    int coinChange(vector<int> &coins, int amount) {
-      int n = coins.size();
-      vector<int>dp(amount+1,-1);
-      int ans = solve(coins,amount,dp);
-      return ans >= 1e9 ? -1 : ans;
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp(amount + 1, -1);
+        int ans = solve(coins, amount, dp);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
 // @leet end
