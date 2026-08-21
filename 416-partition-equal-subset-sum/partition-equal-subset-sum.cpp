b7a53842fec4d4f55fb32d1b5b1fa86a574d1e60
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-08-21 15:29:24 +0530

    Time: 174 ms (20.70%) | Memory: 110.6 MB (34.01%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,25 +1,22 @@
-// @leet imports start
-#include <bits/stdc++.h>
-#include <cstddef>
-using namespace std;
-// @leet imports end
-
-// @leet start
 class Solution {
 private:
     bool rec(int i, int target, vector<int>& nums, vector<vector<int>>& dp) {
+
         if (target == 0)
             return true;
-        if (i == 0)
-            return (nums[i] == target);
+
+        if (i == nums.size())
+            return false;
+
         if (dp[i][target] != -1)
             return dp[i][target];
-        bool notTake = rec(i - 1, target, nums, dp);
+
+        bool notTake = rec(i + 1, target, nums, dp);
 
         bool take = false;
 
         if (nums[i] <= target) {
-            take = rec(i - 1, target - nums[i], nums, dp);
+            take = rec(i + 1, target - nums[i], nums, dp);
         }
 
         return dp[i][target] = (take || notTake);
@@ -27,17 +24,20 @@ private:
 
 public:
     bool canPartition(vector<int>& nums) {
+
         int n = nums.size();
         int sum = 0;
-        for (auto& it : nums) {
+
+        for (auto& it : nums)
             sum += it;
-        }
+
         if (sum % 2 != 0)
             return false;
+
         int target = sum / 2;
 
         vector<vector<int>> dp(n, vector<int>(target + 1, -1));
 
-        return rec(n - 1, target, nums, dp);
+        return rec(0, target, nums, dp);
     }
 };
