3ec69369a9217eb4b64d89589d2e47dfae10d6d6
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-08-25 10:52:05 +0530

    Time: 25 ms (41.01%) | Memory: 29.1 MB (67.88%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -1,34 +1,30 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int m = moveTime.size();
-        int n = moveTime[0].size();
-
         priority_queue<pair<int, pair<int, int>>,
                        vector<pair<int, pair<int, int>>>,
                        greater<pair<int, pair<int, int>>>>
             pq;
 
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        int m = moveTime.size();
+        int n = moveTime[0].size();
+        int INF = 1e7;
 
-        vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-
-        dist[0][0] = 0;
         pq.push({0, {0, 0}});
 
+        vector<pair<int, int>> dirs = {{0, 1}, {-1, 0}, {1, 0}, {0, -1}};
+
+        vector<vector<int>> minTime(m, vector<int>(n, INT_MAX));
+
         while (!pq.empty()) {
-            auto [time, pos] = pq.top();
+            int time = pq.top().first;
+
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
             pq.pop();
 
-            int x = pos.first;
-            int y = pos.second;
-
-            if (time > dist[x][y])
+            if (time > minTime[x][y])
                 continue;
-                
 
             if (x == m - 1 && y == n - 1)
                 return time;
@@ -38,16 +34,15 @@ public:
                 int ny = y + dy;
 
                 if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                    int newTime = max(time, moveTime[nx][ny]) + 1;
+                    int newTime = max(moveTime[nx][ny], time) + 1;
 
-                    if (newTime < dist[nx][ny]) {
-                        dist[nx][ny] = newTime;
+                    if (newTime < minTime[nx][ny]) {
+                        minTime[nx][ny] = newTime;
                         pq.push({newTime, {nx, ny}});
                     }
                 }
             }
         }
-
         return -1;
     }
 };
