1100bee7f1f2907421aa80b66d2acc8eded0f8f2
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-08-28 10:24:11 +0530

    Time: 27 ms (4.75%) | Memory: 31.4 MB (5.24%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    vector<vector<int>> dp;
+    bool solve(string s, int i, int open) {
+        if (open < 0)
+            return 0;
+
+        if (i == s.size()) {
+            return open == 0;
+        }
+        if (dp[i][open] != -1) {
+            return dp[i][open];
+        }
+
+        int ans = false;
+        if (s[i] == '(') {
+            ans = solve(s, i + 1, open + 1);
+
+        } else if (s[i] == ')') {
+            ans = solve(s,i + 1, open - 1);
+
+        } else {
+            bool a = solve(s,i + 1, open - 1);
+            bool b = solve(s,i + 1, open + 1);
+            bool c = solve(s,i + 1, open);
+
+            ans = a | b | c;
+        }
+        return dp[i][open] = ans;
+    }
+
+public:
+    bool checkValidString(string s) {
+        int n = s.size();
+
+        dp.assign(n, vector<int>(n + 1, -1));
+        return solve(s, 0, 0);
+    }
+};
