82bae83d6fbae922ad2976eecee99ba8c2415829
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-09-03 18:03:11 +0530

    Time: 4 ms (83.22%) | Memory: 71.6 MB (46.66%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,34 +1,28 @@
-/*
- * @lc app=leetcode id=1248 lang=cpp
- *
- * [1248] Count Number of Nice Subarrays
- */
-
-// @lc code=start
-#include <unordered_map>
-#include <vector>
-using namespace std;
 class Solution {
-private:
-    unordered_map<int, int> seen;
-    int preCnt = 0;
-    int cnt = 0;
-
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        const int n = nums.size();
-        seen[0] = 1;
+    int atMost(vector<int>& nums, int k) {
+        int left = 0;
+        int odd = 0;
+        int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            if (num % 2 != 0)
-                preCnt++;
-            if (seen.count(preCnt - k)) {
-                cnt += seen[preCnt - k];
+        for (int right = 0; right < nums.size(); right++) {
+
+            if (nums[right] % 2 != 0)
+                odd++;
+
+            while (odd > k) {
+                if (nums[left] % 2 != 0)
+                    odd--;
+
+                left++;
             }
-            seen[preCnt]++;
+
+            ans += right - left + 1;
         }
-        return cnt;
+
+        return ans;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
 };
-// @lc code=end
