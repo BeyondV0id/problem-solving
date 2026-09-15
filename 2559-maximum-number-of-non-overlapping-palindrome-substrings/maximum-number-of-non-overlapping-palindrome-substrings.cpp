452670965c1c6d9f70735a4e25c1e515a583718c
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-09-15 17:23:05 +0530

    Time: 586 ms (12.04%) | Memory: 216.6 MB (13.87%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,19 +1,12 @@
 class Solution {
 private:
     vector<vector<int>> dp;
-
-    bool isPal(string& s, int i, int j) {
-        while (i <= j) {
-            if (s[i] != s[j])
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
+    vector<vector<bool>> isPalindrome;
 
     int backtrack(string& s, int k, int i, int j) {
-        if (j >= s.size())
+        int n = s.size();
+
+        if (i >= n || j >= n)
             return 0;
 
         if (dp[i][j] != -1)
@@ -22,9 +15,8 @@ private:
         int grow = backtrack(s, k, i, j + 1);
         int slide = backtrack(s, k, i + 1, j + 1);
 
-        if (isPal(s, i, j)) {
+        if (isPalindrome[i][j]) {
             int take = 1 + backtrack(s, k, j + 1, j + k);
-
             return dp[i][j] = max(take, max(grow, slide));
         }
 
@@ -34,10 +26,30 @@ private:
 public:
     int maxPalindromes(string s, int k) {
         int n = s.size();
+
         if (k == 1)
             return n;
 
+        if (k > n)
+            return 0;
+
         dp.assign(n + 1, vector<int>(n + 1, -1));
+        isPalindrome.assign(n + 1, vector<bool>(n + 1, false));
+
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L <= n; i++) {
+                int j = i + L - 1;
+
+                if (i == j) {
+                    isPalindrome[i][j] = true;
+                } else if (i + 1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]);
+                } else {
+                    isPalindrome[i][j] =
+                        (s[i] == s[j]) && isPalindrome[i + 1][j - 1];
+                }
+            }
+        }
 
         return backtrack(s, k, 0, k - 1);
     }
